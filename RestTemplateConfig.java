7816d88f1237d0a5e1915010b18db952ede1baba
Vasanth Subramanian <vasant.techie@gmail.com>
2024-06-27 16:04:26 +0530

    Update RestTemplateConfig.java

--- RestTemplateConfig.java
+++ RestTemplateConfig.java
@@ -1,45 +1,27 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService;
-
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
+    private OAuth2AuthorizedClientManager authorizedClientManager;
 
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
 
         restTemplate.getInterceptors().add((request, body, execution) -> {
-            OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
-                "my-client", "your-principal-name");
+            OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(
+                    OAuth2AuthorizeRequest.withClientRegistrationId("my-client")
+                            .principal("your-principal-name")
+                            .build());
 
             if (authorizedClient != null) {
                 OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
